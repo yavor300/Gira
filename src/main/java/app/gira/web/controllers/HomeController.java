d9b62a27294c6d44b4f401983d348ad53da60f3c
Yavor <yavor300@gmail.com>
2021-02-17 20:18:12 +0200

    Initial commit with added index view

---
+++ src/main/java/app/gira/web/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package app.gira.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
